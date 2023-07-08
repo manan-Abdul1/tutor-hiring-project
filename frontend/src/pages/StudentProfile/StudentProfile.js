4dcb4331f9504df9dff77a99ccb8f5c9ee641b23
Abdul Manan <manansheikh117@gmail.com>
2023-07-08 22:55:58 +0500

    Student Profile Updation Done

--- frontend/src/pages/StudentProfile/StudentProfile.js
+++ frontend/src/pages/StudentProfile/StudentProfile.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function StudentProfile() {
   const currentUser = JSON.parse(localStorage.getItem("currentuser"));
   const [name, setName] = useState(currentUser.name);
+  const [email, setEmail] = useState(currentUser.email);
   const [password, setPassword] = useState("");
+  const [studentId, setStudentId] = useState(currentUser.id);
 
   const handleNameChange = (e) => {
     setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
@@ -15,12 +22,27 @@ function StudentProfile() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Name and password updated successfully!");
-    // Update the student's name and password, e.g., send a request to the server or update the local storage
-    // You can use the name and password state values here
-    // Example: localStorage.setItem("currentuser", JSON.stringify({ ...currentUser, name, password }));
-  };
 
+    const updatedStudent = {
+      id: studentId,
+      email: email,
+      password: password,
+      name: name,
+    };
+
+    // Send a PUT request to update the student using Axios
+    axios
+      .put(`http://localhost:5500/api/users/students/${studentId}`, updatedStudent)
+      .then((response) => {
+        console.log("Student updated:", response.data);
+        // Handle any necessary actions after successful update
+        // Example: localStorage.setItem("currentuser", JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.error("Error updating student:", error);
+        // Handle error case
+      });
+  };
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white border border-gray-300 rounded shadow">
       <h1 className="text-2xl font-bold mb-4">Student Profile</h1>
@@ -34,6 +56,18 @@ function StudentProfile() {
             id="name"
             value={name}
             onChange={handleNameChange}
+            className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 focus:border-green-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
             className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-500 focus:border-green-500"
           />
         </div>
