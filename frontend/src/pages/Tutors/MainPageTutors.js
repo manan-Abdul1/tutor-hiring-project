9132c70f86fcc43f9bd3a98f07e9199faf25d7b7
Abdul <manansheikh117@gmail.com>
2023-09-22 01:15:53 +0500

    Remove MainPageTutor component comments

--- frontend/src/pages/Tutors/MainPageTutors.js
+++ frontend/src/pages/Tutors/MainPageTutors.js
@@ -6,27 +6,22 @@ import { useDispatch } from 'react-redux';
 import { addTeachers } from '../../redux/features/teachers/teacherSlice';
 
 export default function MainPageTutors() {
-  // const [teachers, setTeachers] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // Define the API URL
     const apiUrl = 'http://localhost:5500/api/tutors/get-all-teachers';
 
-    // Make a GET request to fetch teacher data
     axios
       .get(apiUrl)
       .then((response) => {
-        // Update the state with the fetched teacher data
         dispatch(addTeachers(response.data.teachers));
         console.log(response.data.teachers)
       })
       .catch((error) => {
-        // Handle any errors that occur during the request
         console.error('Error fetching teacher data:', error);
       });
-  }, []); // The empty dependency array ensures that this effect runs only once on component mount.
-  // console.log(teachers,'teachers')
+  }, []);
+
   return (
     <>
       {/* Pass the fetched teacher data as a prop to the Tutor component */}
