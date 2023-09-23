b9ba0ebe5ec2368dffda5008e74cda80e559373b
Abdul <manansheikh117@gmail.com>
2023-09-24 00:17:07 +0500

    Fix the issue of tutor's not showing on first time

--- frontend/src/pages/Tutors/MainPageTutors.js
+++ frontend/src/pages/Tutors/MainPageTutors.js
@@ -8,8 +8,8 @@ import TutorList from '../../components/TutorList.js/TutorList';
 export default function MainPageTutors() {
   const dispatch = useDispatch();
   const tutors = useSelector((state) => state?.tutor?.teachers);
-  const [filteredTutors, setFilteredTutors] = useState(tutors);
-
+  const [filteredTutors, setFilteredTutors] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     const apiUrl = 'http://localhost:5500/api/tutors/get-all-teachers';
@@ -17,28 +17,31 @@ export default function MainPageTutors() {
       .get(apiUrl)
       .then((response) => {
         dispatch(addTeachers(response.data.teachers));
-        console.log(response.data.teachers)
+        setFilteredTutors(response.data.teachers); // Set filteredTutors with initial data
+        setLoading(false); // Data loading is complete
+        console.log(response.data.teachers);
       })
       .catch((error) => {
         console.error('Error fetching teacher data:', error);
+        setLoading(false); // Even if there's an error, loading is complete
       });
   }, []);
+
   return (
     <>
       <div className="mx-10">
-        <div class="row">
-          <div class="col-md-3">
-            <SearchFilterCard
-              tutors={tutors}
-              setFilteredTutors={setFilteredTutors}
-            />
+        <div className="row">
+          <div className="col-md-3">
+            <SearchFilterCard tutors={tutors} setFilteredTutors={setFilteredTutors} />
           </div>
-          <div class="col-md-9 pt-5">
-            {filteredTutors.length===0||tutors.length === 0 ?
-              <p className='mx-auto flex justify-center items-center h-96 '>No tutors found with the selected filters.</p>
-              :
-              <TutorList tutors={filteredTutors.length > 0 ? filteredTutors: tutors} />
-            }
+          <div className="col-md-9 pt-5">
+            {loading ? (
+              <p className="mx-auto flex justify-center items-center h-96">Loading...</p>
+            ) : filteredTutors.length === 0 ? (
+              <p className="mx-auto flex justify-center items-center h-96">No tutors found with the selected filters.</p>
+            ) : (
+              <TutorList tutors={filteredTutors} />
+            )}
           </div>
         </div>
       </div>
