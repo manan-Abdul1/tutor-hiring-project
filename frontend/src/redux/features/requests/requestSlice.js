ca17015fcd1f8f51ea5ec9894af07707efb1cb5f
Abdul <manansheikh117@gmail.com>
2023-09-28 19:15:57 +0500

    requestSlice is completed

--- frontend/src/redux/features/requests/requestSlice.js
+++ frontend/src/redux/features/requests/requestSlice.js
@@ -1,16 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
-const initialState = {
-    requestData : {},
-}
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    requests: [],
+};
 
 const requestSlice = createSlice({
-  name: 'hiringRequest',
-  initialState,
-  reducers: {
-   
-  },
+    name: "request",
+    initialState,
+    reducers: {
+        getAllRequests: (state, action) => {
+            state.requests = action.payload;
+        },
+        requestAccepted: (state, action) => {
+            state.requests = state.requests.map((request) =>
+                request.id === action.payload.id
+                    ? { ...request, status: "accepted" }
+                    : request
+            );
+        },
+        requestRejected: (state, action) => {
+            state.requests = state.requests.filter(
+                (request) => request.id !== action.payload.id
+            );
+        },
+        getAcceptedRequests: (state) => {
+            return state.requests.filter((request) => request.status === "accepted");
+        },
+    },
 });
 
-export const {  } = requestSlice.actions;
+export const {
+    getAllRequests,
+    requestAccepted,
+    requestRejected,
+    getAcceptedRequests,
+} = requestSlice.actions;
 export default requestSlice.reducer;
