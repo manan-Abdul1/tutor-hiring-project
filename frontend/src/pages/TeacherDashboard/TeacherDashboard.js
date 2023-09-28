d351218a7561a88b23be8918473beabc4b74b1d3
Abdul <manansheikh117@gmail.com>
2023-09-28 19:05:50 +0500

    Style the teachers dashboard

--- frontend/src/pages/TeacherDashboard/TeacherDashboard.js
+++ frontend/src/pages/TeacherDashboard/TeacherDashboard.js
@@ -1,11 +1,12 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const TeacherDashboard = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center mt-32 h-screen">
-      <div className="w-1/4 p-4" onClick={()=>navigate('/teacher-requests')}>
+    <div className="flex flex-wrap justify-center mt-32 h-screen ">
+      <div className="w-full md:w-1/2 lg:w-1/4 p-4" onClick={() => navigate('/teacher-requests')}>
         <div className="bg-white shadow-lg rounded-lg overflow-hidden h-72 transform transition-transform hover:-translate-y-3 hover:text-green-400 hover:cursor-pointer hover:duration-500">
           <div className="p-6">
             <h2 className="text-2xl text-center font-semibold mb-2">Requests</h2>
@@ -13,7 +14,7 @@ const TeacherDashboard = () => {
           </div>
         </div>
       </div>
-      <div className="w-1/4 p-4">
+      <div className="w-full md:w-1/2 lg:w-1/4 p-4">
         <div className="bg-white shadow-lg rounded-lg overflow-hidden h-72 transform transition-transform hover:-translate-y-3 hover:text-green-400 hover:cursor-pointer hover:duration-500">
           <div className="p-6">
             <h2 className="text-2xl text-center font-semibold mb-2">Feedback</h2>
@@ -21,10 +22,10 @@ const TeacherDashboard = () => {
           </div>
         </div>
       </div>
-      <div className="w-1/4 p-4">
+      <div className="w-full md:w-1/2 lg:w-1/4 p-4">
         <div className="bg-white shadow-lg rounded-lg overflow-hidden h-72 transform transition-transform hover:-translate-y-3 hover:text-green-400 hover:cursor-pointer hover:duration-500">
           <div className="p-6">
-            <h2 className="text-2xl text-center font-semibold mb-2">Messages</h2>
+            <h2 className="text-2xl text-center font-semibold mb-2">Meetings</h2>
             {/* Add your content for the new card here */}
           </div>
         </div>
