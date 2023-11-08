4ff2bcec4aa6d10db47311fc93e4c828b9b26562
Sohaib56 <syedsohaibali346@gmail.com>
2023-11-08 23:28:35 +0500

    centering map

--- frontend/src/components/Hire Input Field/MapLocationIq.js
+++ frontend/src/components/Hire Input Field/MapLocationIq.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-const MapLocationIq = ({setPhysicalLocation}) => {
+const MapLocationIq = ({ setPhysicalLocation }) => {
   const [location, setLocation] = useState(null);
   const [searchValue, setSearchValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
@@ -11,7 +11,7 @@ const MapLocationIq = ({setPhysicalLocation}) => {
   const provider = new OpenStreetMapProvider({
     params: {
       countrycodes: 'pk',
-      language: 'en', 
+      language: 'en',
     },
   });
 
@@ -40,79 +40,72 @@ const MapLocationIq = ({setPhysicalLocation}) => {
     }
   }, [searchValue]);
 
-  const MapComponent = () => {
+  const MapComponent = ({ center }) => {
     const map = useMap();
-    if (location) {
-        map.flyTo([location.y, location.x], 18);
+    if (center) {
+      map.flyTo(center, 18);
     }
     return null;
   };
 
   return (
     <div>
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: '33%' }}>
-          <input
-            type="text"
-            value={searchValue}
-            onChange={handleSearchChange}
-            placeholder="Search Location"
-            style={{
-              width: '100%',
-              padding: '8px',
-              border: '1px solid #ccc',
-              borderRadius: '4px',
-            }}
-          />
-          {suggestions.length > 0 && !suggestionSelected && (
-            <ul
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleSearchChange}
+        placeholder="Search Location"
+        style={{
+          width: '100%',
+          padding: '8px',
+          border: '1px solid #ccc',
+          borderRadius: '4px',
+        }}
+      />
+      {suggestions.length > 0 && !suggestionSelected && (
+        <ul
+          style={{
+            marginTop: '10px',
+            padding: '0',
+            listStyle: 'none',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            background: 'white',
+          }}
+        >
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion.place_id}
+              onClick={() => handleSelectSuggestion(suggestion)}
               style={{
-                marginTop: '10px',
-                padding: '0',
-                listStyle: 'none',
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-                background: 'white',
+                cursor: 'pointer',
+                padding: '8px',
+                ':hover': {
+                  background: '#f0f0f0',
+                },
               }}
             >
-              {suggestions.map((suggestion) => (
-                <li
-                  key={suggestion.place_id}
-                  onClick={() => handleSelectSuggestion(suggestion)}
-                  style={{
-                    cursor: 'pointer',
-                    padding: '8px',
-                    ':hover': {
-                      background: '#f0f0f0',
-                    },
-                  }}
-                >
-                  {suggestion.label}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div style={{ width: '67%' }}>
-          <MapContainer
-            center={[31.5204, 74.3587]}
-            zoom={11}
-            style={{ height: '50vh', width: '100%' }}
-          >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              accessToken='pk.017ee18c650ce4549a3402c52847140f'
-            />
-            {location && (
-              <Marker position={[location.y, location.x]}>
-                <Popup>{location.label}</Popup>
-              </Marker>
-            )}
-            <MapComponent />
-          </MapContainer>
-        </div>
-      </div>
+              {suggestion.label}
+            </li>
+          ))}
+        </ul>
+      )}
+      <MapContainer
+        zoom={11}
+        style={{ height: '30vh', width: '100%' }}
+        center={location ? [location.y, location.x] : [31.5204, 74.3587]}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {location && (
+          <Marker position={[location.y, location.x]}>
+            <Popup>{location.label}</Popup>
+          </Marker>
+        )}
+        <MapComponent center={location ? [location.y, location.x] : [31.5204, 74.3587]} />
+      </MapContainer>
     </div>
   );
 };
