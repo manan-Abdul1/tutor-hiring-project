b41e174942fea2602c9063d383d52084838a29c6
Sohaib56 <syedsohaibali346@gmail.com>
2023-11-12 16:32:44 +0500

    Tutor Register Validation

--- frontend/src/pages/Register/StepOne/StepOne.js
+++ frontend/src/pages/Register/StepOne/StepOne.js
@@ -1,5 +1,201 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { toast } from 'react-hot-toast';
+
+// const PersonalInformation = ({ formData, setFormData, navigation }) => {
+//   const { name, phone, email, cnic, password, confirmPassword, address, gender, age } = formData;
+//   const { next } = navigation;
+
+//   const handleNext = (e) => {
+//     e.preventDefault();
+
+//     if (password !== confirmPassword) {
+//       toast.error('Passwords do not match');
+//       return;
+//     }
+//     next();
+//   }
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+
+//     if (name === 'gender') {
+//       setFormData((prevData) => ({
+//         ...prevData,
+//         [name]: value === 'male' ? 'male' : 'female',
+//       }));
+//     } else {
+//       setFormData((prevData) => ({
+//         ...prevData,
+//         [name]: value,
+//       }));
+//     }
+//   }
+
+//   return (
+//     <>
+//       <form className="bg-white rounded-lg flex flex-col shadow-2xl max-w-2xl p-4 mx-auto items-center">
+//         <h1 className="text-2xl text-center text-green-700 font-bold mb-4">Personal Information</h1>
+//         <div className="grid w-full grid-cols-2 gap-4 mb-4">
+//           <div>
+//             <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name: *</label>
+//             <input
+//               id="name"
+//               name="name"
+//               type="text"
+//               autoComplete='off'
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={name}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">Phone: *</label>
+//             <input
+//               id="phone"
+//               name="phone"
+//               type="text"
+//               autoComplete='off'
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={phone}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//         </div>
+//         <div className="grid w-full grid-cols-2 gap-4 mb-4">
+//           <div>
+//             <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email: *</label>
+//             <input
+//               id="email"
+//               name="email"
+//               type="email"
+//               autoComplete='off'
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={email}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="cnic" className="block text-gray-700 font-bold mb-2">CNIC: *</label>
+//             <input
+//               id="cnic"
+//               name="cnic"
+//               type="text"
+//               autoComplete='off'
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={cnic}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//         </div>
+//         <div className="grid w-full grid-cols-2 gap-4 mb-4">
+//           <div>
+//             <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password: *</label>
+//             <input
+//               id="password"
+//               name="password"
+//               type="password"
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={password}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="confirmPassword" className="block text-gray-700 font-bold mb-2">Confirm Password: *</label>
+//             <input
+//               id="confirmPassword"
+//               name="confirmPassword"
+//               type="password"
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={confirmPassword}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//         </div>
+//         <div className="mb-4 w-full">
+//           <label htmlFor="address" className="block text-gray-700 font-bold mb-2">Address: *</label>
+//           <input
+//             id="address"
+//             name="address"
+//             type="text"
+//             autoComplete='off'
+//             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//             value={address}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <div className="grid grid-cols-2 w-full gap-4 mb-4">
+//           <div className='flex items-center'>
+//             <label htmlFor="gender" className="block text-gray-700 font-bold mb-2 mx-2">Gender: </label>
+//             <div className='pb-1'>
+//               <input
+//                 type="radio"
+//                 id="male"
+//                 name="gender"
+//                 value="male"
+//                 checked={gender === 'male'}
+//                 onChange={handleInputChange}
+//               />
+//               <label htmlFor="male" className="ml-2 mr-4">Male</label>
+//               <input
+//                 type="radio"
+//                 id="female"
+//                 name="gender"
+//                 value="female"
+//                 checked={gender === 'female'}
+//                 onChange={handleInputChange}
+//               />
+//               <label htmlFor="female" className="ml-2">Female</label>
+//             </div>
+//           </div>
+//           <div>
+//             <label htmlFor="age" className="block text-gray-700 font-bold mb-2">Age: *</label>
+//             <input
+//               id="age"
+//               name="age"
+//               type="text"
+//               autoComplete='off'
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               value={age}
+//               onChange={handleInputChange}
+//             />
+//           </div>
+//         </div>
+//         <button
+//           className="bg-blue-500 ml-auto mt-2 text-right hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+//           onClick={handleNext}
+//         >
+//           Next
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default PersonalInformation;
+
+import React from 'react';
 import { toast } from 'react-hot-toast';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+  name: yup.string()
+  .required('Name is required')
+  .matches(/^[A-Z][a-zA-Z]*(\s[a-zA-Z][a-zA-Z]*)*$/, 'Invalid name format'),
+phone: yup.string().required('Phone is required'),
+email: yup.string().email('Invalid email address').required('Email is required'),
+cnic: yup.string().required('CNIC is required'),
+password: yup.string()
+  .min(8, 'Password must be at least 8 characters')
+  .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .matches(/[0-9]/, 'Password must contain at least one numeric digit')
+  .required('Password is required'),
+confirmPassword: yup.string()
+  .oneOf([yup.ref('password'), null], 'Passwords must match')
+  .required('Confirm Password is required'),
+address: yup.string().required('Address is required'),
+gender: yup.string().required('Gender is required'),
+age: yup.string().required('Age is required'),
+});
 
 const PersonalInformation = ({ formData, setFormData, navigation }) => {
   const { name, phone, email, cnic, password, confirmPassword, address, gender, age } = formData;
@@ -8,12 +204,19 @@ const PersonalInformation = ({ formData, setFormData, navigation }) => {
   const handleNext = (e) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      toast.error('Passwords do not match');
-      return;
-    }
-    next();
-  }
+    validationSchema
+      .validate(formData, { abortEarly: false })
+      .then(() => {
+        if (password !== confirmPassword) {
+          toast.error('Passwords do not match');
+        } else {
+          next();
+        }
+      })
+      .catch((error) => {
+        error.errors.forEach((errorMessage) => toast.error(errorMessage));
+      });
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -29,16 +232,17 @@ const PersonalInformation = ({ formData, setFormData, navigation }) => {
         [name]: value,
       }));
     }
-  }
+  };
 
   return (
     <>
       <form className="bg-white rounded-lg flex flex-col shadow-2xl max-w-2xl p-4 mx-auto items-center">
+        {/* ... (same as your previous JSX) */}
         <h1 className="text-2xl text-center text-green-700 font-bold mb-4">Personal Information</h1>
-        <div className="grid w-full grid-cols-2 gap-4 mb-4">
-          <div>
-            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name: *</label>
-            <input
+         <div className="grid w-full grid-cols-2 gap-4 mb-4">
+           <div>
+             <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name: *</label>
+             <input
               id="name"
               name="name"
               type="text"
@@ -169,6 +373,8 @@ const PersonalInformation = ({ formData, setFormData, navigation }) => {
       </form>
     </>
   );
-}
+};
 
 export default PersonalInformation;
+
+
--- frontend/src/pages/Register/StepTwo/StepTwo.js
+++ frontend/src/pages/Register/StepTwo/StepTwo.js
@@ -1,13 +1,199 @@
-import React from 'react';
+// import React from 'react';
+
+// const StepTwo = ({ formData, setFormData, navigation }) => {
+//   const { previous, next } = navigation;
+
+//   const handleInputChange = event => {
+//     const { name, value } = event.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value
+//     });
+//   };
+
+//   return (
+//     <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
+//       <div className="px-6 py-4">
+//         <h2 className="text-2xl text-green-700 font-bold mb-3 text-center">About Teaching</h2>
+//         <form>
+//           <div className="grid grid-cols-2 gap-4 mb-4">
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="timing">
+//                 Timing:
+//               </label>
+//               <select
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 name="timing"
+//                 id="timing"
+//                 value={formData.timing}
+//                 onChange={handleInputChange}
+//               >
+//                 <option value="">Select Timing</option>
+//                 <option value="morning">Morning</option>
+//                 <option value="afternoon">Afternoon</option>
+//                 <option value="evening">Evening</option>
+//                 <option value="night">Night</option>
+//               </select>
+//             </div>
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="experience">
+//                 Experience:
+//               </label>
+//               <input
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 type="text"
+//                 name="experience"
+//                 id="experience"
+//                 value={formData.experience}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//           </div>
+//           <div className="grid grid-cols-2 gap-4 mb-4">
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="currentTeachInstitute">
+//                 Current Institute (Teaching):
+//               </label>
+//               <input
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 type="text"
+//                 name="currentTeachInstitute"
+//                 id="currentTeachInstitute"
+//                 value={formData.currentTeachInstitute}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="currentStudyInstitute">
+//                 Alumni:
+//               </label>
+//               <input
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 type="text"
+//                 name="alumni"
+//                 id="alumni"
+//                 value={formData.alumni}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//           </div>
+//           <div className="grid grid-cols-2 gap-4 mb-4">
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="education">
+//                 Education:
+//               </label>
+//               <input
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 type="text"
+//                 name="education"
+//                 id="education"
+//                 value={formData.education}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div>
+//               <label className="block text-gray-700 font-bold mb-2" htmlFor="city">
+//                 City:
+//               </label>
+//               <input
+//                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 type="text"
+//                 name="city"
+//                 id="city"
+//                 value={formData.city}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//           </div>
+//           <div className="mb-4">
+//             <label className="block text-gray-700 font-bold mb-2" htmlFor="details">
+//               Details (Bio):
+//             </label>
+//             <textarea
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               name="bio"
+//               id="bio"
+//               rows="5"
+//               value={formData.bio}
+//               onChange={handleInputChange}
+//             ></textarea>
+//           </div>
+//           <div className="flex items-center justify-between">
+//             <button
+//               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+//               onClick={previous}
+//             >
+//               Previous
+//             </button>
+//             <button
+//               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+//               onClick={next}
+//             >
+//               Next
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default StepTwo;
+
+
+
+import React, { useState } from 'react';
+import * as Yup from 'yup';
+import { toast } from 'react-hot-toast';
+
+const validationSchema = Yup.object({
+  timing: Yup.string().required('Timing is required'),
+  experience: Yup.string().required('Experience is required'),
+  currentTeachInstitute: Yup.string().required('Current Teaching Institute is required'),
+  alumni: Yup.string().required('Alumni is required'),
+  education: Yup.string().required('Education is required'),
+  city: Yup.string().required('City is required'),
+  bio: Yup.string().required('Bio is required'),
+});
 
 const StepTwo = ({ formData, setFormData, navigation }) => {
   const { previous, next } = navigation;
+  const [errors, setErrors] = useState({});
 
-  const handleInputChange = event => {
+  const validateForm = async () => {
+    try {
+      await validationSchema.validate(formData, { abortEarly: false });
+      setErrors({});
+      return true;
+    } catch (error) {
+      const newErrors = {};
+      error.inner.forEach((fieldError) => {
+        newErrors[fieldError.path] = fieldError.message;
+      });
+      setErrors(newErrors);
+      return false;
+    }
+  };
+
+  const handleNext = async (e) => {
+    e.preventDefault();
+    const isValid = await validateForm();
+
+    if (isValid) {
+      next();
+    } else {
+      // Display specific error messages for each field
+      Object.keys(errors).forEach((fieldName) => {
+        toast.error(`${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}: ${errors[fieldName]}`);
+      });
+    }
+  };
+
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -22,7 +208,9 @@ const StepTwo = ({ formData, setFormData, navigation }) => {
                 Timing:
               </label>
               <select
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors['timing'] ? 'border-red-500' : ''
+                }`}
                 name="timing"
                 id="timing"
                 value={formData.timing}
@@ -34,90 +222,114 @@ const StepTwo = ({ formData, setFormData, navigation }) => {
                 <option value="evening">Evening</option>
                 <option value="night">Night</option>
               </select>
+              {errors['timing'] && <p className="text-red-500 text-xs mt-1">{errors['timing']}</p>}
             </div>
             <div>
               <label className="block text-gray-700 font-bold mb-2" htmlFor="experience">
                 Experience:
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  errors['experience'] ? 'border-red-500' : ''
+                }`}
                 type="text"
                 name="experience"
                 id="experience"
                 value={formData.experience}
                 onChange={handleInputChange}
               />
+              {errors['experience'] && <p className="text-red-500 text-xs mt-1">{errors['experience']}</p>}
             </div>
           </div>
+
           <div className="grid grid-cols-2 gap-4 mb-4">
-            <div>
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="currentTeachInstitute">
-                Current Institute (Teaching):
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                name="currentTeachInstitute"
-                id="currentTeachInstitute"
-                value={formData.currentTeachInstitute}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="currentStudyInstitute">
-                Alumni:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                name="alumni"
-                id="alumni"
-                value={formData.alumni}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4 mb-4">
-            <div>
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="education">
-                Education:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                name="education"
-                id="education"
-                value={formData.education}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="city">
-                City:
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                name="city"
-                id="city"
-                value={formData.city}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="details">
-              Details (Bio):
-            </label>
-            <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="bio"
-              id="bio"
-              rows="5"
-              value={formData.bio}
-              onChange={handleInputChange}
-            ></textarea>
-          </div>
+  <div>
+    <label className="block text-gray-700 font-bold mb-2" htmlFor="currentTeachInstitute">
+      Current Institute (Teaching):
+    </label>
+    <input
+      className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+        errors['currentTeachInstitute'] ? 'border-red-500' : ''
+      }`}
+      type="text"
+      name="currentTeachInstitute"
+      id="currentTeachInstitute"
+      value={formData.currentTeachInstitute}
+      onChange={handleInputChange}
+    />
+    {errors['currentTeachInstitute'] && (
+      <p className="text-red-500 text-xs mt-1">{errors['currentTeachInstitute']}</p>
+    )}
+  </div>
+  <div>
+    <label className="block text-gray-700 font-bold mb-2" htmlFor="alumni">
+      Alumni:
+    </label>
+    <input
+      className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+        errors['alumni'] ? 'border-red-500' : ''
+      }`}
+      type="text"
+      name="alumni"
+      id="alumni"
+      value={formData.alumni}
+      onChange={handleInputChange}
+    />
+    {errors['alumni'] && <p className="text-red-500 text-xs mt-1">{errors['alumni']}</p>}
+  </div>
+</div>
+<div className="grid grid-cols-2 gap-4 mb-4">
+  <div>
+    <label className="block text-gray-700 font-bold mb-2" htmlFor="education">
+      Education:
+    </label>
+    <input
+      className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+        errors['education'] ? 'border-red-500' : ''
+      }`}
+      type="text"
+      name="education"
+      id="education"
+      value={formData.education}
+      onChange={handleInputChange}
+    />
+    {errors['education'] && <p className="text-red-500 text-xs mt-1">{errors['education']}</p>}
+  </div>
+  <div>
+    <label className="block text-gray-700 font-bold mb-2" htmlFor="city">
+      City:
+    </label>
+    <input
+      className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+        errors['city'] ? 'border-red-500' : ''
+      }`}
+      type="text"
+      name="city"
+      id="city"
+      value={formData.city}
+      onChange={handleInputChange}
+    />
+    {errors['city'] && <p className="text-red-500 text-xs mt-1">{errors['city']}</p>}
+  </div>
+</div>
+<div className="mb-4">
+  <label className="block text-gray-700 font-bold mb-2" htmlFor="bio">
+    Details (Bio):
+  </label>
+  <textarea
+    className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+      errors['bio'] ? 'border-red-500' : ''
+    }`}
+    name="bio"
+    id="bio"
+    rows="5"
+    value={formData.bio}
+    onChange={handleInputChange}
+  ></textarea>
+  {errors['bio'] && <p className="text-red-500 text-xs mt-1">{errors['bio']}</p>}
+</div>
+
+          
           <div className="flex items-center justify-between">
             <button
               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
@@ -127,7 +339,7 @@ const StepTwo = ({ formData, setFormData, navigation }) => {
             </button>
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={next}
+              onClick={handleNext}
             >
               Next
             </button>
