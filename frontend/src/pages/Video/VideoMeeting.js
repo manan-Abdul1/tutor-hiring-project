de1e8380203d51f7bfda0e57e7588b373852df35
Abdul <manansheikh117@gmail.com>
2023-11-14 23:28:34 +0500

    Update the video handle if one user left, end the meeting and check meeting Ended

--- frontend/src/pages/Video/VideoMeeting.js
+++ frontend/src/pages/Video/VideoMeeting.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 import Peer from "simple-peer";
 import io from "socket.io-client";
@@ -9,88 +11,121 @@ const VideoMeeting = () => {
     const { videoId } = useParams();
     const [peer, setPeer] = useState();
     const [stream, setStream] = useState();
-	const [ callEnded, setCallEnded] = useState(false)
+    const [callEnded, setCallEnded] = useState(false);
+    const [isVideoEnded, setIsVideoEnded] = useState(false);
     const navigate = useNavigate();
     const myVideo = useRef();
-  
-    useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
-          setStream(stream);
-          myVideo.current.srcObject = stream;
-      
-          // Join the meeting room.
-          socket.emit("joinRoom", videoId);
-      
-          // Listen for other users joining the meeting room.
-          socket.on("allUsers", (users) => {
-            // If the user is the initiator, create a new peer connection and signal the other users.
-            if (users.length > 1) {
-              const newPeer = new Peer({
-                initiator: true,
-                trickle: false,
-                stream,
-              });
-      
-              newPeer.on("signal", (signal) => {
-                socket.emit("sendingSignal", { userToSignal: users[0], callerID: socket.id, signal });
-              });
-      
-              setPeer(newPeer);
-            }
-          });
-      
-          // Listen for other users signaling to the user.
-          socket.on("userJoined", (payload) => {
-            // If the user is not the initiator, accept the signal from the other user and create a new peer connection.
-            if (payload.callerID !== socket.id) {
-              const newPeer = new Peer({
-                initiator: false,
-                trickle: false,
-                stream,
-              });
-      
-              newPeer.on("signal", (signal) => {
-                socket.emit("returningSignal", { signal, callerID: payload.callerID });
-              });
-      
-              newPeer.signal(payload.signal);
-      
-              setPeer(newPeer);
-            }
-          });
-      
-          // Listen for the peer connection to receive data from other users.
-          socket.on("receivingReturnedSignal", (payload) => {
-            // Add the other user's video and audio tracks to the user's peer connection.
-            peer.addTrack(payload.stream.getVideoTracks()[0]);
-            peer.addTrack(payload.stream.getAudioTracks()[0]);
-          });
-        });
-      
-        // Destroy the peer connection when the component is unmounted.
-        return () => {
-            if (peer) {
-              peer.destroy();
-              setPeer(null);
-            }
-          };
-      }, [videoId]);
-      
-  const endMeeting = async () =>{
-    try {
-        setCallEnded(true);
-        navigate('/home');
-        window.location.reload();
-    } catch (error) {
-        console.log(error)
-    }
 
-  }
+    useEffect(() => {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+        setStream(stream);
+        myVideo.current.srcObject = stream;
+
+        // Join the meeting room.
+        socket.emit("joinRoom", videoId);
+
+        // Listen for other users joining the meeting room.
+        socket.on("allUsers", (users) => {
+          // If the user is the initiator, create a new peer connection and signal the other users.
+          if (users.length > 1) {
+            const newPeer = new Peer({
+              initiator: true,
+              trickle: false,
+              stream,
+            });
+
+            newPeer.on("signal", (signal) => {
+              socket.emit("sendingSignal", { userToSignal: users[0], callerID: socket.id, signal });
+            });
+
+            setPeer(newPeer);
+          }
+        });
+
+        // Listen for other users signaling to the user.
+        socket.on("userJoined", (payload) => {
+          // If the user is not the initiator, accept the signal from the other user and create a new peer connection.
+          if (payload.callerID !== socket.id) {
+            const newPeer = new Peer({
+              initiator: false,
+              trickle: false,
+              stream,
+            });
+
+            newPeer.on("signal", (signal) => {
+              socket.emit("returningSignal", { signal, callerID: payload.callerID });
+            });
+
+            newPeer.signal(payload.signal);
+
+            setPeer(newPeer);
+          }
+        });
+
+        // Listen for the peer connection to receive data from other users.
+        socket.on("receivingReturnedSignal", (payload) => {
+          // Add the other user's video and audio tracks to the user's peer connection.
+          peer.addTrack(payload.stream.getVideoTracks()[0]);
+          peer.addTrack(payload.stream.getAudioTracks()[0]);
+        });
+      });
+
+      // Destroy the peer connection when the component is unmounted.
+      return () => {
+        if (peer) {
+          peer.destroy();
+          setPeer(null);
+        }
+      };
+    }, [videoId]);
+
+    const endMeeting = async () => {
+      try {
+        await axios
+          .put(`http://localhost:5500/api/hiringRequest/endVideo?videoId=${videoId}`)
+          .then((response) => {
+            toast.success(response.data.message);
+            setCallEnded(true);
+            navigate('/home');
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error("Error fetching notifications:", error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const checkVideoEnd = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5500/api/hiringRequest/isVideoEnded?videoId=${videoId}`);
+        setIsVideoEnded(response.data.ok && response.data.isVideoEnded);
+        if(isVideoEnded){
+          navigate('/home')
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error("Error fetching video status:", error);
+      }
+    };
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        checkVideoEnd();
+      }, 5000); 
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [videoId]);
+
     return (
       <div className="container">
         <div className="video-container">
           <div className="video">
-            {stream && !callEnded && <video playsInline  ref={myVideo} autoPlay style={{ width: "300px" }} />}
+            {stream && !callEnded && !isVideoEnded && <video playsInline ref={myVideo} autoPlay style={{ width: "300px" }} />}
+            {isVideoEnded && <p>The meeting has ended. Leave the meeting.</p>}
           </div>
           {peer && !callEnded && (
             <div className="video">
@@ -102,7 +137,5 @@ const VideoMeeting = () => {
       </div>
     );
   };
-  
 
-  
   export default VideoMeeting;
